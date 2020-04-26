78c02de854678028fbb3ccb928b7e46dae0131a5
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-04-26 17:00:24 +0200

    adds Helmet and font

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,15 +1,24 @@
 import React from "react"
 import SideBar from "../side-bar/side-bar"
 import style from "./layout.module.css"
+import { Helmet } from "react-helmet"
+
 const Layout = props => {
   return (
-    <div>
+    <>
+      <Helmet title="spanisch" defer={false}>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+
       <aside>
         <SideBar location={props.location} />
       </aside>
 
       <main className={style.layoutMain}>{props.children}</main>
-    </div>
+    </>
   )
 }
 
