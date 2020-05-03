141192ecca0a509e0940648de2ff3af040a1583f
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-05-03 21:06:31 +0200

    ejercicio unidad 6

---
+++ src/exercise-builders/select/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import { answer as cssAnswer } from "../input/input.module.css"
+
+const SelectComp = ({ root, options, answer }) => {
+  const [selectValue, setSelectValue] = useState("   ")
+
+  const handleChange = e => {
+    setSelectValue(e.target.value)
+  }
+
+  const renderTextOrSelect = () => {
+    if (selectValue === answer) {
+      return <span className={cssAnswer}>{answer}</span>
+    }
+    return (
+      <label htmlFor="prueba">
+        <select value={selectValue} id="prueba" onChange={handleChange}>
+          <option value={selectValue}>{selectValue}</option>
+          {options.map(option => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </label>
+    )
+  }
+
+  return renderTextOrSelect()
+}
+
+export default SelectComp
