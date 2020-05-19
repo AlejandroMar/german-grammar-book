cd18307723ba8ce59fe0d94e9570e98b6fdf8860
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-05-19 13:57:00 +0200

    change name of site package

--- packages/gatsby-theme-language-book/src/components/layout/layout.js
+++ packages/gatsby-theme-language-book/src/components/layout/layout.js
@@ -1,12 +1,12 @@
-import React from "react"
-import SideBar from "../side-bar/side-bar"
-import style from "./layout.module.css"
-import { Helmet } from "react-helmet"
+import React from 'react'
+import SideBar from '../side-bar/side-bar'
+import style from './layout.module.css'
+import { Helmet } from 'react-helmet'
 
 const Layout = props => {
   return (
     <>
-      <Helmet title="spanisch-online" defer={false}>
+      <Helmet title="the language book" defer={false}>
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
           rel="stylesheet"
