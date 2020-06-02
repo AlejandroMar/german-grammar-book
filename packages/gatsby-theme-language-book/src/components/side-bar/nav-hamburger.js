73f922eddae37cb1ede20ef18d3cdd2145e60e36
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-06-02 09:24:37 +0200

    clean css

--- packages/gatsby-theme-language-book/src/components/side-bar/nav-hamburger.js
+++ packages/gatsby-theme-language-book/src/components/side-bar/nav-hamburger.js
@@ -3,9 +3,9 @@ import style from './side-bar.module.css'
 
 const NavHamburger = ({ openNavbarNav }) => (
   <div onClick={openNavbarNav} className={style.navBarButton}>
-    <span></span>
-    <span></span>
-    <span></span>x
+    <span>{''}</span>
+    <span>{''}</span>
+    <span>{''}</span>
   </div>
 )
 export default NavHamburger
