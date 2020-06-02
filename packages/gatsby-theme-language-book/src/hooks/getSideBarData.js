344359e5c32780f3a7a82bb06b5cbc9e6a019e42
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-06-02 06:42:34 +0200

    creates hook to get sideBarData

---
+++ packages/gatsby-theme-language-book/src/hooks/getSideBarData.js
@@ -0,0 +1,29 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+export default () => {
+  const sideBarData = useStaticQuery(graphql`
+    query MyQuery {
+      allMenuLink {
+        edges {
+          node {
+            name
+            path
+            items {
+              name
+              path
+              items {
+                name
+                path
+                items {
+                  name
+                  path
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return sideBarData
+}
