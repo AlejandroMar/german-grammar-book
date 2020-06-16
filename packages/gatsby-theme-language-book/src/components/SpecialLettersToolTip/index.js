0a5390b52b331a85db9aec27d46ad8f35af3dd27
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-06-16 09:02:54 +0200

    factor out SpecialLettersToolTip

---
+++ packages/gatsby-theme-language-book/src/components/SpecialLettersToolTip/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(theme => ({
+  letterTool: {
+    position: 'absolute',
+    listStyle: 'none',
+    marginTop: '2px',
+    width: '7rem',
+    padding: '0',
+    zIndex: '1200',
+    backgroundColor: theme.palette.info.light,
+    opacity: '.8',
+    borderRadius: theme.shape.borderRadius,
+    boxShadow: theme.shadows[1],
+  },
+
+  letterToolItem: {
+    textAlign: 'center',
+    float: 'left',
+    width: '1rem',
+    cursor: 'pointer',
+
+    '&:hover': {
+      backgroundColor: 'white',
+    },
+  },
+}));
+
+const SpecialLettersToolTip = ({ fun, letters }) => {
+  const classes = useStyles();
+  return (
+    <ul className={classes.letterTool}>
+      {letters.map((letter, i) => (
+        <li
+          className={classes.letterToolItem}
+          key={i}
+          onMouseDown={e => fun(e)}
+        >
+          {letter}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SpecialLettersToolTip;
