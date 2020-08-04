67cef3853dd2658e82a37ebea4061f620a62192a
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-08-04 11:29:29 +0200

    adds manifest and favicon.png

--- esra/gatsby-config.js
+++ esra/gatsby-config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: ['gatsby-theme-language-book'],
+  plugins: [
+    'gatsby-theme-language-book',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Idiomatica School`,
+        short_name: `Idiomatica`,
+        start_url: `/`,
+        display: `standalone`,
+        icon: `images/favicon.png`,
+      },
+    },
+  ],
 }
